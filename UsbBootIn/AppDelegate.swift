478738000905edb5b5c655a47bba94027981aad2
Ronald Brandon Moore <btraemoore@gmail.com>
2016-07-10 21:32:20 -0500

    Initial Commit

---
+++ UsbBootIn/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UsbBootIn
+//
+//  Created by Trae Moore on 7/10/16.
+//  Copyright © 2016 Trae Moore. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UsbBootIn/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UsbBootIn
+//
+//  Created by Trae Moore on 7/10/16.
+//  Copyright © 2016 Trae Moore. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
